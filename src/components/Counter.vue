60ded1a5243f3174f9d91eb5f102d5aae99b0e46
Tanvir Alam <ta@casalogic.dk>
2017-05-14 01:36:04 +0200

    Simple counter with hot module replacement

---
+++ src/components/Counter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>The counter is: {{ count }}</h2>
+        <p>
+            <button @click="count += 1">Increment</button>
+        </p>
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+           return { count: 0 };
+        }
+    }
+</script>
